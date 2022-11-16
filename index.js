2506edf81bb44acf9673cc8d2589e160594305b8
Jayakrishnan <thatcoder.space@gmail.com>
2022-11-16 11:54:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const fnblock = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello fnblock`}))
+  res.end()
+  
+}
+
+export default fnblock
